20de87e17206cde5678dd9367fe07c62df98fc71
Mahdi Sadeghzadeh Ghamsary <p_roufarshbaf@yahoo.com>
2023-04-10 00:21:11 +0330

    Add main function.

---
+++ server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+		"github.com/labstack/echo/v4"
+	api "github.com/parhamrou/Music-CloudStorage/server/endpoints"
+	db  "github.com/parhamrou/Music-CloudStorage/server/database"
+)
+
+func main() {
+	e := echo.New()
+	initialize(e)
+	e.Logger.Printf("Listenning on port 8080...\n")
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+// This method connects to the database and defines http methods and their handlers
+func initialize(e *echo.Echo) error {
+	if err := db.Connect(); err != nil {
+		return err
+	}
+	e.POST("/api/music", api.AddMusic)
+	e.GET("/api/music/:music_name", api.GetMusic)
+	e.DELETE("api/music/:music_name", api.DeleteMusic)
+	e.GET("/api/album/:album_name", api.GetByAlbum)
+	e.GET("api/artist/:artist_name", api.GetByArtist)
+	return nil
+}
