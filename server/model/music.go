e19ae45fef251979e936516bd630793decb7ac46
Mahdi Sadeghzadeh Ghamsary <p_roufarshbaf@yahoo.com>
2023-04-10 01:35:01 +0330

    Change Music struct.

--- server/model/music.go
+++ server/model/music.go
@@ -2,10 +2,9 @@ package model
 
 // This is a struct for all musics. The value field is actually base64URL of music binary 
 type Music struct {
-	FileName	string 	`json:"file_name"`
+	ID 			string  `json:"id"`
 	Title		string 	`json:"title"`
 	Album 		string 	`json:"album"`
 	Artist		string  `json:"artist"`
-	Value		string  `json:"value"`
 }
 
